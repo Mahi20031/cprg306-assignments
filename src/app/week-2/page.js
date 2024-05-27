fe2f75abe8c524489556c7d32844e106e3d72edb
Mahi20031 <123599134+Mahi20031@users.noreply.github.com>
2024-05-27 17:19:37 -0600

    week-2

---
+++ src/app/week-2/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import StudentInfo from './student-info'
+
+function page() {
+  return (
+    <div>
+      <h1>
+        Shopping List
+      </h1>
+      <StudentInfo />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>
+        Mahipinder Ahluwalia
+      </h1>
+      <Link href="/">GitHub URL</Link>
+    </div>
+  )
+}
+
+export default StudentInfo
