f5f27ada52ca6e933b1c4e18c6793f05fcf4f98a
Mahi20031 <123599134+Mahi20031@users.noreply.github.com>
2024-06-05 15:12:50 -0600

    week-3

---
+++ src/app/week-3/item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Item = ({name, quantity, category}) => {
+    return(
+        <li>
+            <span>{name}</span>
+            <span>{quantity}</span>
+            <span>{category}</span>
+        </li>
+    )
+}
+export default Item;
