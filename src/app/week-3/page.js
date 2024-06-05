d27a4b9d33a0684da74a311bfd0f0d906bdc4def
Mahi20031 <123599134+Mahi20031@users.noreply.github.com>
2024-06-05 15:26:38 -0600

    mistakes changed

--- src/app/week-3/page.js
+++ src/app/week-3/page.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import itemList from './item-list';
+import ItemList from './item-list';
 
 const Page = () => {
   return (
     <main>
       <h1>Shopping List</h1>
-      <itemList />
+      <ItemList />
     </main>
   );
 };
