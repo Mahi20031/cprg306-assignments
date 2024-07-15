6a76c451030d6c9556ed7dd310af8f147d0d54fb
Mahi20031 <123599134+Mahi20031@users.noreply.github.com>
2024-07-14 19:22:33 -0600

    e

--- src/app/week-7/item.js
+++ src/app/week-7/item.js
@@ -1,11 +1,12 @@
-"use client";
-
-import React from 'react';
-
-export default function Item({ name, quantity, category, onSelect }) {
-    return (
-        <li onClick={() => onSelect(name)}>
-            {name} - {quantity} ({category})
-        </li>
-    );
+export default function Item({ item, onSelect }) {
+  return (
+    <ul
+      className="border border-3 p-2 m-5 bg-yellow-200 text-black"
+      onClick={() => onSelect(item)}
+    >
+      <li className="text-4xl font-bold">{item.name}</li>
+      <li className="text-sm ">Category: {item.category}</li>
+      <li className="text-sm ">Quantity: {item.quantity}</li>
+    </ul>
+  );
 }
